f55d0a3cf2efb4cc809cf48c73b1e662842f5672
Oladoja Qudus <oladojaqudus50@gmail.com>
2022-09-08 15:27:42 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - the beginning of the code
  *
- * return - the end of the code
+ * return: 0 if success
  */
 int main(void)
 {
 	char new[52] = "\"Programming is like building a multilingual puzzle";
 	puts(new);
-	return 0;
+	return (0);
 }
