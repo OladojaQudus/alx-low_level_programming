61665136feedc8e7df09ecf6927fa81d3c2657d7
Oladoja Qudus <oladojaqudus50@gmail.com>
2022-09-08 15:00:51 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(void)
+
+{
+
+		//string initialisation
+		   char Mystr[] = "\"Programming is like building a multilingual puzzle";
+		            puts(Mystr); 
+		    	    return 0;
+}                    
