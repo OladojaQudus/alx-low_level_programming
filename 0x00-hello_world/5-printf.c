7a0e27b7e2151b6e06df7c83f615b4f7da1c5fd9
Oladoja Qudus <oladojaqudus50@gmail.com>
2022-09-08 16:36:16 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - the beginning of the code
+ * main -the beginning of the program
  *
  * Return: 0 if success
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, ");
+	printf("with proper grammar, but the outcome is a piece of art,");
 
 	return (0);
 }
