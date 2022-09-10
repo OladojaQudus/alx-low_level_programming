7b6a6917840dbcc199cac1b045ab634052cf0851
Oladoja Qudus <oladojaqudus50@gmail.com>
2022-09-10 14:17:11 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - the proggramme prints lower case alphabets except q and e
+ *
+ * Return: 0 for succes
+ */
+
+int main(void)
+
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+		c++;
+		putchar('\n');
+
+		return (0);
+	}
+
+}
