0054eff4a7dce7db18091b2c1d8c4db36b0d2352
Oladoja Qudus <oladojaqudus50@gmail.com>
2022-09-10 09:51:27 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	printf("size of an int: %li byte(s)\n", sizeof(int));
 	printf("size of a long int: %li byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %ld byte(s)\n", sizeof(long long int));
-	printf("size of a float: %ld byte(s)", sizeof(float));
+	printf("size of a float: %ld byte(s)\n", sizeof(float));
 
 	return (0);
 }
