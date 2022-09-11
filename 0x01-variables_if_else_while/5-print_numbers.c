288ec59f268624364042de3eadc865b041611e75
Oladoja Qudus <oladojaqudus50@gmail.com>
2022-09-11 07:25:21 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints the numbers from 0-9
+ *
+ * Return: end of the program
+ */
+
+int main(void)
+
+{
+
+	printf("0123456789\n");
+
+	return (0);
+}
