80d04c0290c81ccf2f3b77b59582525afcf93e38
Oladoja Qudus <oladojaqudus50@gmail.com>
2022-09-11 07:40:06 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - the programm prints single numbers
+ *
+ * Return: the end of the program
+ */
+
+int main(void)
+
+{
+	int i = '0';
+		while (i <= '9')
+		{
+			putchar(i);
+			i = i++;
+
+		}
+		putchar('\n');
+
+			return (0);
+}
