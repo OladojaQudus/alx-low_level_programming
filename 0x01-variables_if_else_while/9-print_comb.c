55be18fa3097cf7d5fcb558533f8783910b9100f
Oladoja Qudus <oladojaqudus50@gmail.com>
2022-09-12 12:53:29 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - the program prints all combination of single digit number
+ * main - the program prints single number
  *
  * Return: 0 if success
  */
-
 int main(void)
 {
 	int n;
 
-	for (n = 47; n < 58; n++)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
-
 		if (n != 57)
 		{
 			putchar(',');
@@ -21,6 +19,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
