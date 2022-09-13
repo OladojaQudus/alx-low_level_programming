5523afd809e98120630fc9acb2ac8163c9ef263e
Oladoja Qudus <oladojaqudus50@gmail.com>
2022-09-13 04:44:06 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - this a program that prints the putchar
+ *
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
