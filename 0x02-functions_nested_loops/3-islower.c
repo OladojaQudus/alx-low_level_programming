116b112c74a42425a52be4602d3a12cb9e9fe1b2
Oladoja Qudus <oladojaqudus50@gmail.com>
2022-09-13 07:47:54 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - vhcks for lower case
+ *
+ * c - the charactr to test
+ * Return: 0 if false
+ */
+
+int _islower(int c)
+{
+
+	c = _islower('H');
+	_putchar(c + '0');
+
+	_putchar('\n');
+	c = _islower('o');
+	_putchar(c + '0');
+	_putchar('\n');
+	return (0);
+}
