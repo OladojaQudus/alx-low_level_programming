d1dce65e0f33d6d13ee4ce61106433322051ee64
Oladoja Qudus <oladojaqudus50@gmail.com>
2022-09-13 13:13:15 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * add - the program adds two numbers
+ *
+ * @i: first numbr
+ * @j: second number
+ * Return: 0
+ */
+
+int add(int i, int j)
+{
+	int k;
+
+	k = i + j;
+
+	if (k < 0)
+	{
+		k = -k;
+	}
+	putchar((k / 10) + '0');
+	putchar((k % 10) + '0');
+}
