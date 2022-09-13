bfa0da17d4cd3e29f7f8a6ad2da3c04dd5878e26
Oladoja Qudus <oladojaqudus50@gmail.com>
2022-09-13 13:01:47 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,6 +12,7 @@ void times_table(void)
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
+		{
 			c = a * b;
 		if (b != 0)
 		{
@@ -30,6 +31,9 @@ void times_table(void)
 		}
 		else
 			_putchar((c % 10) + '0');
+		}
+		_putchar('\n');
+
 	}
-	_putchar('\n');
+
 }
