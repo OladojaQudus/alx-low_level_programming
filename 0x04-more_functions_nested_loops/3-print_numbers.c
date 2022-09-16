05646ad40f268ad9502aaf4f292b29d113a7cce6
Oladoja Qudus <oladojaqudus50@gmail.com>
2022-09-16 09:16:50 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - to print letters
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		putchar(i);
+	}
+	i++;
+	putchar('\n');
+}
