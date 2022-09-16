c3eb4776bf32d52eb785edd319157bfbbf83d175
Oladoja Qudus <oladojaqudus50@gmail.com>
2022-09-16 09:36:25 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print some numbers
+ *
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	if (i <= 9 && (i != 2 || i != 4))
+	{
+		_putchar(i);
+		i++;
+	}
+}
