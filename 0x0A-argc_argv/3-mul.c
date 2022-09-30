05edc2b549be4ce2ec9558d8f552c18f5e1a4725
Oladoja Qudus <oladojaqudus50@gmail.com>
2022-09-30 09:27:38 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry
+ * @argc: count argument
+ * @argv: argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc == 3)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a * b);
+	}
+	printf("Error\n");
+	return (0);
+}
